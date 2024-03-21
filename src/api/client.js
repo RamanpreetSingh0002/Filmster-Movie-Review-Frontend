1cca2763e6f745bd397286727653a2af2e0d7396
Ramanpreet Singh <singhramanpreet964@gmail.com>
2024-03-21 19:49:19 +0530

    changed base url in client.js

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // http://localhost:8000/api ---> instead of this use live server link after deploying
 const client = axios.create({
-  baseURL: "https://filmster-backend.netlify.app/api",
+  baseURL: "https://filmster-movie-review-backend.onrender.com/api",
 });
 
 export default client;
