28c6ebf83ac73f51bb065e5203b785a7b192bfae
Ramanpreet Singh <singhramanpreet964@gmail.com>
2024-04-02 23:31:51 +0530

    profile modal change

--- src/models/ProfileModal.jsx
+++ src/models/ProfileModal.jsx
@@ -5,6 +5,7 @@ import { useNotification } from "../hooks";
 
 const ProfileModal = ({ visible, profileId, onClose }) => {
   const [profile, setProfile] = useState({});
+  const [busy, setBusy] = useState(false);
 
   const { updateNotification } = useNotification();
 
@@ -22,15 +23,17 @@ const ProfileModal = ({ visible, profileId, onClose }) => {
   const { avatar, name, about } = profile;
 
   return (
-    <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
-      <div className="w-72 p-5 rounded flex flex-col items-center bg-white dark:bg-primary space-y-3">
-        <img className="w-28 h-28 rounded-full" src={avatar} alt="" />
-        <h1 className="dark:text-white text-primary font-semibold">{name}</h1>
-        <p className="dark:text-dark-subtle text-light-subtle text-justify">
-          {about}
-        </p>
-      </div>
-    </ModalContainer>
+    !busy && (
+      <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
+        <div className="w-72 p-5 rounded flex flex-col items-center bg-white dark:bg-primary space-y-3">
+          <img className="w-28 h-28 rounded-full" src={avatar} alt="" />
+          <h1 className="dark:text-white text-primary font-semibold">{name}</h1>
+          <p className="dark:text-dark-subtle text-light-subtle text-justify">
+            {about}
+          </p>
+        </div>
+      </ModalContainer>
+    )
   );
 };
 
